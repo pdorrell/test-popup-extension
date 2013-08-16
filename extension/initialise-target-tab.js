fdef795ddfb50218ece8b47812710f7d3dfe9ad9
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-08-17 09:28:44 +1200

    TitledWindow constructor & prototype

--- extension/initialise-target-tab.js
+++ extension/initialise-target-tab.js
@@ -2,28 +2,47 @@ function inspect(object) {
   return JSON.stringify(object);
 }
 
-var messageHandler = {
+function TitledWindow(window, $) {
+  this.window = window;
+  this.$ = $;
+}
+
+TitledWindow.prototype = {
   getPageDetails: function(request, sendResponse) {
     console.log("getTitle ...");
-    var title = $("title").text();
+    var title = this.$("title").text();
     console.log("title from jquery = " + inspect(title));
-    sendResponse({title: title, url: window.location.href});
+    sendResponse({title: title, url: this.window.location.href});
   }, 
+  
   setWindowTitle: function(request, sendResponse) {
     console.log("setWindowTitle ...");
-    $("title").text(request.title);
-    sendResponse({title: $("title").text()}); // return updated title
+    this.$("title").text(request.title);
+    sendResponse({title: this.$("title").text()}); // return updated title
+  }
+};
+
+var titledWindow = new TitledWindow(window, $);
+
+var titledWindowMessageHandler = {
+  getPageDetails: function(request, sendResponse) {
+    titledWindow.getPageDetails(request, sendResponse);
+  }, 
+  setWindowTitle: function(request, sendResponse) {
+    titledWindow.setWindowTitle(request, sendResponse);
   }
 }
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log("runtime message " + inspect(request));
-  var handler = messageHandler[request.type];
-  if (handler) {
-    handler(request, sendResponse);
-  }
-  return true;
-});
+function handleTitleWindowRequests() {
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log("runtime message " + inspect(request));
+    var handler = titledWindowMessageHandler[request.type];
+    if (handler) {
+      handler(request, sendResponse);
+    }
+    return true;
+  });
+}
 
 function createPopupWindowViaChromeRuntime() {
   chrome.runtime.sendMessage({type: "openPopupWindow"}, 
@@ -33,7 +52,11 @@ function createPopupWindowViaChromeRuntime() {
 }
 
 function initialise() {
-  createPopupWindowViaChromeRuntime();
+  if (chrome && chrome.runtime) {
+    console.log("Initialising target tab for chrome extension");
+    handleTitleWindowRequests();
+    createPopupWindowViaChromeRuntime();
+  }
 }
 
 initialise();
