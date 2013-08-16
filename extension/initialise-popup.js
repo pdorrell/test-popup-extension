dc269cef555b67ffcc23fb54f81651fd0f8169a3
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-08-17 09:57:12 +1200

    TitledWindowProxy

--- extension/initialise-popup.js
+++ extension/initialise-popup.js
@@ -2,32 +2,49 @@ function inspect(object) {
   return JSON.stringify(object);
 }
 
-var targetTabId = null;
+var titledWindow = null;
+
+function TitledWindowProxy(tabId) {
+  this.tabId = tabId;
+}
+
+TitledWindowProxy.prototype = {
+  getPageDetails: function(handleResult) {
+    chrome.tabs.sendMessage(this.tabId, 
+                            {type: "getPageDetails"}, 
+                            function(result) { handleResult(result); });
+  }, 
+  setWindowTitle: function(title, handleResult) {
+    chrome.tabs.sendMessage(this.tabId, {type: "setWindowTitle", title: title}, 
+                            function(result) { handleResult(result); });
+  }
+};
 
 function updateTitle(targetTitle) {
   $("title").text("Test Popup: " + targetTitle);
 }  
 
 function updateTargetTabTitle() {
-  chrome.tabs.sendMessage(targetTabId, {type: "setWindowTitle", title: $("#title").val()}, 
-                          function(response) {
-                            updateTitle(response.title);
-                            console.log("updateTargetTabTitle, response = " + inspect(response));
-                          });
+  titledWindow.setWindowTitle($("#title").val(), 
+                              function(result) {
+                                updateTitle(result.title);
+                                console.log("updateTargetTabTitle, result = " + inspect(result));
+                              });
 }
 
 function initialise() {
-  targetTabId = parseInt(window.location.search.substring(1));
+  var targetTabId = parseInt(window.location.search.substring(1));
+  titledWindow = new TitledWindowProxy(targetTabId);
+  
   console.log("targetTabId = " + inspect(targetTabId));
   $("#targetTabId").text(targetTabId);
-  chrome.tabs.sendMessage(targetTabId,
-                          {type: "getPageDetails"}, 
-                          function(result) {
-                            console.log("getPageDetails result = " + inspect(result));
-                            $("#title").val(result.title);
-                            $("#url").text(result.url);
-                            updateTitle(result.title);
-                          });
+  
+  titledWindow.getPageDetails(function(result) {
+    console.log("getPageDetails result = " + inspect(result));
+    $("#title").val(result.title);
+    $("#url").text(result.url);
+    updateTitle(result.title);
+  });
   
   $("#title").on("keypress", function(event, ui) {
       if (event.which == 13) {
